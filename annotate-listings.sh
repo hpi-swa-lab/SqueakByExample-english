92338c2750f2b5daf106db0f1a37b1faa2a81268
Christoph Thiede <38782922+LinqLover@users.noreply.github.com>
2019-11-05 19:59:13 +0100

    Fix another bug in annotate-listings.sh
    Extension methods were not respecteed. Also mentioned in #17.

--- annotate-listings.sh
+++ annotate-listings.sh
@@ -1,5 +1,5 @@
 for file in $(find ListingSources -type f -name "*.st"); do
-	if [[ $file =~ package\/(.*)\.class\/ ]]; then
+	if [[ $file =~ package\/(.*)\.(class|extension)\/ ]]; then
 		echo -e "${BASH_REMATCH[1]}>>>$(cat $file)" > $file
 	fi
 done
