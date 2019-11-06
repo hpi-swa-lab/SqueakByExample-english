1395caa626036a316353c09527bf478459e20842
LinqLover <christoph.thiede@student.hpi.de>
2019-11-06 12:53:00 +0100

    Rework deploy-debug.sh: Branch specific file name, refactoring

--- deploy-debug.sh
+++ deploy-debug.sh
@@ -2,27 +2,45 @@
 # Inspired by https://joeyrobert.org/2016/07/13/artifact-deployment-via-google-drive/
 
 if [ -z "$1" ]; then
-	read -p "File to export? " TARGET
+	read -p "File to export? " SOURCE
 else
-	TARGET=$1
+	SOURCE=$1
 fi
 
 ./travis_fold -start
-echo -e "\e[96mI will now export the build file $TARGET to Google Drive."
+echo -e "\e[96mUpload of ${SOURCE} to Google Drive started."
 
+GDRIVE=./bin/gdrive
 # Note: This token may expire after some time. Just create a fake account and update the token below to reactive it :P
 GDRIVE_REFRESH_TOKEN="1//09E6QalRd0WHbCgYIARAAGAkSNwF-L9IrA4CQiIhYB_RUnaJYYy9h76ShYfqYLioZdIyZMpD9x4replF0JYuRZRLfwHL1PybqdpI"
-GDRIVE_FILE="1JND416mbf-zEHH9ojUt4IGzI5YSQH9GS"
+GDRIVE_FOLDER="1tNIvN-9Vx8djNZYfSYuqhjheb-EgJuTc"
 GDRIVE_URL="https://drive.google.com/drive/folders/1tNIvN-9Vx8djNZYfSYuqhjheb-EgJuTc?usp=sharing"
 
-echo "Downloading gdrive ..."
-GDRIVE_URL='https://docs.google.com/uc?id=0B3X9GlR6EmbnQ0FtZmJJUXEyRTA&export=download'
-mkdir -p "bin"
-curl -fSL "${GDRIVE_URL}" -o ./bin/gdrive --progress-bar
-chmod +x ./bin/gdrive
+install_gdrive() {
+	echo "Downloading gdrive ..."
+	GDRIVE_URL='https://docs.google.com/uc?id=0B3X9GlR6EmbnQ0FtZmJJUXEyRTA&export=download'
+	mkdir -p $(dirname $GDRIVE_URL)
+	curl -fSL "${GDRIVE_URL}" -o $GDRIVE --progress-bar
+	chmod +x $GDRIVE
+}
 
-FIXED_BRANCH=$(echo $BRANCH | sed 's/\//-/g')
-./bin/gdrive update $GDRIVE_FILE --refresh-token $GDRIVE_REFRESH_TOKEN $TARGET
+find_file() {
+	$GDRIVE list -q "'${GDRIVE_FOLDER}' in parents and name = '$1' and trashed = false" --no-header | awk '{print $1}'
+}
+
+if [ ! -f $GDRIVE ]; then install_gdrive; fi
+
+FIXED_BRANCH=$(echo $TRAVIS_BRANCH | sed 's/\//-/g')
+FILE_NAME="SBE-$FIXED_BRANCH.pdf"
+GDRIVE_FILE=$(find_file $FILE_NAME)
+if [[ $GDRIVE_FILE ]]; then
+	echo "Uploading new version of ${FILE_NAME} ..."
+	$GDRIVE update --refresh-token $GDRIVE_REFRESH_TOKEN --name $FILE_NAME $GDRIVE_FILE $SOURCE
+else
+	echo "Uploading ${FILE_NAME} ..."
+	$GDRIVE upload --name $FILE_NAME $SOURCE -p $GDRIVE_FOLDER
+	GDRIVE_FILE=$(find_file $FILE_NAME)
+fi
 
 ./travis_fold -end
 
