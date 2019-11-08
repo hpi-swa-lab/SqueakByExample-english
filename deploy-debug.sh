2e2528a75b2a7e72e1068496818de75fffca4d44
LinqLover <christoph.thiede@student.hpi.de>
2019-11-08 15:07:44 +0100

    deploy-debug: Add missing gdrive refresh token

--- deploy-debug.sh
+++ deploy-debug.sh
@@ -13,48 +13,32 @@ echo -e "\e[96mUpload of ${SOURCE} to Google Drive started."
 GDRIVE=./bin/gdrive
 # Note: This token may expire after some time. Just create a fake account and update the token below to reactive it :P
 GDRIVE_REFRESH_TOKEN="1//09E6QalRd0WHbCgYIARAAGAkSNwF-L9IrA4CQiIhYB_RUnaJYYy9h76ShYfqYLioZdIyZMpD9x4replF0JYuRZRLfwHL1PybqdpI"
+GDRIVE_ARGS="--refresh-token ${GDRIVE_REFRESH_TOKEN}"
 GDRIVE_FOLDER="1tNIvN-9Vx8djNZYfSYuqhjheb-EgJuTc"
 GDRIVE_URL="https://drive.google.com/drive/folders/1tNIvN-9Vx8djNZYfSYuqhjheb-EgJuTc?usp=sharing"
 
 install_gdrive() {
-	echo "Downloading gdrive ..."
 	GDRIVE_URL='https://docs.google.com/uc?id=0B3X9GlR6EmbnQ0FtZmJJUXEyRTA&export=download'
 	mkdir -p $(dirname $GDRIVE)
-	echo " Will curl"
 	curl -fSL "${GDRIVE_URL}" -o $GDRIVE --progress-bar
-	echo " Did curl"
 	chmod +x $GDRIVE
-	echo " Did chmod"
 }
 
 function find_file {
-	echo " enter find_file"
-	echo " finding file $1"
-	echo " query is '${GDRIVE_FOLDER}' in parents and name = '$1' and trashed = false"
-	echo " result is:"
-	echo $($GDRIVE list -q "'${GDRIVE_FOLDER}' in parents and name = '$1' and trashed = false" --no-header)
-	echo " ."
 	$GDRIVE list -q "'${GDRIVE_FOLDER}' in parents and name = '$1' and trashed = false" --no-header | awk '{print $1}'
 }
 
 if [ ! -f $GDRIVE ]; then install_gdrive; fi
-echo " after fi"
 
 FIXED_BRANCH=$(echo $TRAVIS_BRANCH | sed 's/\//-/g')
-echo " fixed branch is ${FIXED_BRANCH}"
 FILE_NAME="SBE-$FIXED_BRANCH.pdf"
-echo " file name is ${FILE_NAME}"
-echo " debug find_file"
-find_file $FILE_NAME
-echo " debugged findfile"
 GDRIVE_FILE=$(find_file $FILE_NAME)
-echo " gdrive_file is ${GDRIVE_FILE}"
 if [[ $GDRIVE_FILE ]]; then
 	echo "Uploading new version of ${FILE_NAME} ..."
-	$GDRIVE update --refresh-token $GDRIVE_REFRESH_TOKEN --name $FILE_NAME $GDRIVE_FILE $SOURCE
+	$GDRIVE update $GDRIVE_ARGS --name $FILE_NAME $GDRIVE_FILE $SOURCE
 else
 	echo "Uploading ${FILE_NAME} ..."
-	$GDRIVE upload --name $FILE_NAME $SOURCE -p $GDRIVE_FOLDER
+	$GDRIVE upload $GDRIVE_ARGS --name $FILE_NAME $SOURCE -p $GDRIVE_FOLDER
 	GDRIVE_FILE=$(find_file $FILE_NAME)
 fi
 
