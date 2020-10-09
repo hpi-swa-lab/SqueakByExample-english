6099dc263243d7998e940f4d7e438fd7664395c9
Christoph Thiede <38782922+LinqLover@users.noreply.github.com>
2020-10-09 10:45:26 +0200

    Fix travis release builds (#41)
    This PR reworks the Travis CI configuration more or less completely. The main motivation was to resolve double releases and build errors introduced via #38 since which we have been having two separate jobs for different Squeak versions.

--- annotate-listings.sh
+++ annotate-listings.sh
@@ -1,22 +1,22 @@
 #!/bin/bash -e 
-if [ -z $1 ] || [ -z $2 ]; then
-	echo "usage: $(basename $0) <source_directory> <target_directory>"
+if [ -z "$1" ] || [ -z "$2" ]; then
+	echo "usage: $(basename "$0") <source_directory> <target_directory>"
 	exit
 fi
 
 SOURCE=$1
-if [ ! -d $SOURCE ]; then
+if [ ! -d "$SOURCE" ]; then
 	>&2 echo "No valid source directory specified"
 	exit 1
 fi
 TARGET=$2
 
-mkdir -p $TARGET
+mkdir -p "$TARGET"
 
 for file in $(find $SOURCE -type f -name "*.st"); do
 	if [[ $file =~ package\/(.*)\.(class|extension)\/ ]]; then
 		new_file="${TARGET}/$(cut -sd / -f2- <<< "$file")"
-		mkdir -p $(dirname $new_file)
-		echo -e "${BASH_REMATCH[1]}>>>$(tail -n +2 "$file")" > $new_file
+		mkdir -p "$(dirname "$new_file")"
+		echo -e "${BASH_REMATCH[1]}>>>$(tail -n +2 "$file")" > "$new_file"
 	fi
 done
